d08969d003918edcb0dec1f7784a4df9090ec15d
Nicole Apono <napononicole@gmail.com>
2025-03-13 10:24:28 +0300

    writing functions to perform the operation

---
+++ basic_calculator.py
@@ -0,0 +1,16 @@
+def basic_calculator(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+# # User imput
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# operator = input("Enter operator (+, -, *, /): ")
